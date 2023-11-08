9eda39239800888fe02a0c19e06ac652f2ccf64f
unknown <dimassetyonugroho30@gmail.com>
2023-11-08 14:15:50 +0700

    melakukan percobaan for, mengganti pengujian for dengan <= 4

--- arrayBilangan08.java
+++ arrayBilangan08.java
@@ -8,10 +8,33 @@ public class arrayBilangan08 {
         bil[2] = -7;
         bil[3] = 17;
 
+        //3. ubah menjadi perulangan for
+        for(int i = 0; i < 4; i++ ) {
+            System.out.println(bil[i]);
+        }
+        //4.menggantii <=4
+        for(int i = 0; i <= 4; i++ ) {
+            System.out.println(bil[i]);
+        }
+
         // /Tampilkan ke layar semua isi elemennya
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
+
+        // //masing-masing elemen array bil diubah dengan angka 5.0, 12867, 7.5, 2000000. 
+        // bil[0] = 5.0;
+        // bil[1] = 12867;
+        // bil[2] = 7.5;
+        // bil[3] = 2000000;
+
+        // //melakukan inisialisasi elemen array sekaligus pada saat deklarasi array.
+        // int[] array = {5, 13, -7, 17};
+        // System.out.println(array[0]);
+        // System.out.println(array[1]);
+        // System.out.println(array[2]);
+        // System.out.println(array[3]);
+
     }
 }
