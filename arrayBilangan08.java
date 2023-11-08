908e5168840a56a43789f044f95ce10f2cdfadd9
unknown <dimassetyonugroho30@gmail.com>
2023-11-08 13:06:20 +0700

    push array bilangan pada percobaan pertama

---
+++ arrayBilangan08.java
@@ -0,0 +1,17 @@
+public class arrayBilangan08 {
+    public static void main(String[] args) {
+        
+        int[] bil = new int[4];
+        //Isi masing-masing elemen array bil tadi dengan angka 5, 13, -7, 17.
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        // /Tampilkan ke layar semua isi elemennya
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
