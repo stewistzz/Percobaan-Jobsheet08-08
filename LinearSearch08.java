6e2ff60a23dc822a2821f0023b744fa526baeb03
unknown <dimassetyonugroho30@gmail.com>
2023-11-11 20:21:43 +0700

    program awal linear search

---
+++ LinearSearch08.java
@@ -0,0 +1,18 @@
+public class LinearSearch08 {
+    public static void main(String[] args) {
+        
+        int[] arrayInt = {34, 18,26, 48, 72, 20, 56,63};
+        int key = 20, hasil = 0;
+
+        // pengkondisian for
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+
+    }
+}
