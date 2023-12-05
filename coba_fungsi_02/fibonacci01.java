5def990081c8ccc881df52c466295f4dc4869fcc
unknown <dimassetyonugroho30@gmail.com>
2023-12-05 20:40:38 +0700

    menambahkan fibonacci

---
+++ coba_fungsi_02/fibonacci01.java
@@ -0,0 +1,18 @@
+package coba_fungsi_02;
+
+import java.util.Scanner;
+
+public class fibonacci01 {
+    public static void main(String[] args) {
+        int f-n, f-n-1, f-n-2, n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        n = input.nextInt();
+        f-n-2 = 0;
+        f-n-1 = 1;
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(n);
+        }
+    }
+}
